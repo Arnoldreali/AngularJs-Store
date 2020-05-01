024004fdc5f3f7510e24e4752258f5d3473f9ad1
Arnoldreali <55991934+Arnoldreali@users.noreply.github.com>
2020-05-01 12:26:34 -0600

    clase del viernes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('store', [])
+    var app = angular.module('store', ['store-directives'])
 
     
     app.controller("ReviewController", function(){
@@ -10,235 +10,208 @@
       }
     })
     app.controller('StoreController', function(){
-      this.products = [
-      { 
-        name:"Dodecahedron",
-        price: 2,
-        description: "Dodecahedron is very hard to broke, its a gem very nice",
-        specs: [
-          {
-          faces: 14,
-          color: "#000",
-          rarity: 10,
-          shine: 30
-        }
-      ],
-        images:[
-                {
-                  full: "gem-01.gif",
-                  thumb:'gem-01.gif'
-                },
-                {
-                    full:'gem-01.gif',
-                    thumb:'gem-01.gif'
-                  }
-                ],
-        canPurchase: true,
-        soldOut: false,
-        reviews:[
-                  {
-                    stars: 5,
-                    body:"I love this product!",
-                    author:"Arnold@homas.com"
-                  },
-                  {
-                    stars:1,
-                    body:"This product sucks",
-                    author:"Reali@hater.com",
-                  }
-                ]
-      }, 
-      {
-        name:"Pentagonal Gem",
-        price: 5.95,
-        description: "this gem is so good to have fun",
-        specs: [
-          {
-          faces: 5,
-          color: "#000",
-          rarity: 6,
-          shine: 10
-        }
-      ],
-        images:[
-                  {
-                    full: "gem-02.gif",
-                    thumb:'gem-02.gif'
-                  },
-                  {
-                    full:'gem-02.gif',
-                    thumb:'gem-02.gif'
-                  }
-                ],
-        canPurchase: true,
-        soldOut: false,
-        reviews:[
-                  {
-                    stars: 5,
-                    body:"I love this product!",
-                    author:"Arnold@homas.com",
-                  },
-                  {
-                    stars:1,
-                    body:"This product sucks",
-                    author:"Reali@hater.com",
-                  }
-                ]
-      },
-      {
-        name:"Azurite",
-        price: 110.5,
-        description: "Some gems have hidden qualities beyond their luster.",
-        specs: [
-          {
-          faces: 14,
-          color: "#000",
-          rarity: 7,
-          shine: 8
-        }
-      ],
-        images:[
-                  {
-                    full: "gem-03.gif",
-                    thumb:'gem-03.gif'
-                  },
-                  {
-                    full:'gem-03.gif',
-                    thumb:'gem-03.gif'
-                  }
-                ],
-        canPurchase: true,
-        soldOut: false,
-        reviews:[
-                  {
-                    stars: 5,
-                    body:"I love this product!",
-                    author:"Arnold@homas.com",
-                  },
-                  {
-                    stars:1,
-                    body:"This product sucks",
-                    author:"Reali@hater.com",
-                  },
-                  {
-                    "stars": 5,
-                    "body": "I love this gem!",
-                    "author": "joe@example.org",
-                  }
-                ]
-      },
-      {
-        name:"Bloodstone",
-        price: 22.9,
-        description: "Origin of the Bloodstone is unknown, hence its low value.",
-        specs: [
-          {
-          faces: 12,
-          color: "#000",
-          rarity: 6,
-          shine: 9
-        }
-      ],
-        images:[
-                  {
-                    full: "gem-04.gif",
-                    thumb:'gem-04.gif'
-                  },
-                  {
-                    full:'gem-04.gif',
-                    thumb:'gem-04.gif'
-                  }
-                ],
-        canPurchase: true,
-        soldOut: false,
-        reviews:[
-                  {
-                    stars: 5,
-                    body:"I love this product!",
-                    author:"Arnold@homas.com"
-                  },
-                  {
-                    stars:1,
-                    body:"This product sucks",
-                    author:"Reali@hater.com"
-                  },
-                  {
-                    stars: 3,
-				            body: "I think this gem was just OK, could honestly use more shine, IMO.",
-				            author: "JimmyDean@example.org",
-                  }
-                ]
-      },
-      {
-        name:"Zircon",
-        price: 1100,
-        description: "Zircon is our most coveted and sought after gem.",
-        specs: [
-          {
-          faces: 6,
-          color: "#000",
-          rarity: 2,
-          shine: 70
-        }
-      ],
-        images:[
-                  {
-                    full: "gem-05.gif",
-                    thumb:'gem-05.gif'
-                  },
-                  {
-                    full:'gem-05.gif',
-                    thumb:'gem-05.gif'
-                  }
-                ],
-        canPurchase: true,
-        soldOut: false,
-        reviews:[
-                  {
-                    stars: 5,
-                    body:"I love this product!",
-                    author:"Arnold@homas.com"
-                  },
-                  {
-                    stars:1,
-                    body:"This product sucks",
-                    author:"Reali@hater.com"
-                  },
-                  {
-                    stars: 1,
-				            body: "This gem is WAY too expensive for its rarity value.",
-				            author: "turtleguyy@example.org",
-                  }
-                ]
-      }
-  ] 
+      this.products = gems
+     
 })
-app.directive('product', function(){
-  return{
-    restrict: 'E',
-    templateUrl: 'product.html'
+let gems =[ { 
+  name:"Dodecahedron",
+  price: 2,
+  description: "Dodecahedron is very hard to broke, its a gem very nice",
+  specs: [
+    {
+    faces: 14,
+    color: "#000",
+    rarity: 10,
+    shine: 30
   }
-})
-app.directive('productPanels', function(){
-  return{
-    restrict: 'E',
-    templateUrl:'product-panels.html',
-    controller:function(){
-      
-    },
-    controllerAs: 'panel'
+],
+  images:[
+          {
+            full: "./img/gem-01.gif",
+            thumb:'./img/gem-01.gif'
+          },
+          {
+              full:'./img/gem-01.gif',
+              thumb:'./img/gem-01.gif'
+            }
+          ],
+  canPurchase: true,
+  soldOut: false,
+  reviews:[
+            {
+              stars: 5,
+              body:"I love this product!",
+              author:"Arnold@homas.com"
+            },
+            {
+              stars:1,
+              body:"This product sucks",
+              author:"Reali@hater.com",
+            }
+          ]
+}, 
+{
+  name:"Pentagonal Gem",
+  price: 5.95,
+  description: "this gem is so good to have fun",
+  specs: [
+    {
+    faces: 5,
+    color: "#000",
+    rarity: 6,
+    shine: 10
   }
-})
-
-app.controller("PanelController", function(){
-  this.tab=1
-  
-  this.selectTab = function(setTab){
-    this.tab = setTab
+],
+  images:[
+            {
+              full: "./img/gem-02.gif",
+              thumb:'./img/gem-02.gif'
+            },
+            {
+              full:'./img/gem-02.gif',
+              thumb:'./img/gem-02.gif'
+            }
+          ],
+  canPurchase: true,
+  soldOut: false,
+  reviews:[
+            {
+              stars: 5,
+              body:"I love this product!",
+              author:"Arnold@homas.com",
+            },
+            {
+              stars:1,
+              body:"This product sucks",
+              author:"Reali@hater.com",
+            }
+          ]
+},
+{
+  name:"Azurite",
+  price: 110.5,
+  description: "Some gems have hidden qualities beyond their luster.",
+  specs: [
+    {
+    faces: 14,
+    color: "#000",
+    rarity: 7,
+    shine: 8
   }
-  this.isSelected = function(checkTab){
-    return this.tab === checkTab
+],
+  images:[
+            {
+              full: "./img/gem-03.gif",
+              thumb:'./img/gem-03.gif'
+            },
+            {
+              full:'./img/gem-03.gif',
+              thumb:'./img/gem-03.gif'
+            }
+          ],
+  canPurchase: true,
+  soldOut: false,
+  reviews:[
+            {
+              stars: 5,
+              body:"I love this product!",
+              author:"Arnold@homas.com",
+            },
+            {
+              stars:1,
+              body:"This product sucks",
+              author:"Reali@hater.com",
+            },
+            {
+              "stars": 5,
+              "body": "I love this gem!",
+              "author": "joe@example.org",
+            }
+          ]
+},
+{
+  name:"Bloodstone",
+  price: 22.9,
+  description: "Origin of the Bloodstone is unknown, hence its low value.",
+  specs: [
+    {
+    faces: 12,
+    color: "#000",
+    rarity: 6,
+    shine: 9
   }
-})
-
+],
+  images:[
+            {
+              full: "./img/gem-04.gif",
+              thumb:'./img/gem-04.gif'
+            },
+            {
+              full:'./img/gem-04.gif',
+              thumb:'./img/gem-04.gif'
+            }
+          ],
+  canPurchase: true,
+  soldOut: false,
+  reviews:[
+            {
+              stars: 5,
+              body:"I love this product!",
+              author:"Arnold@homas.com"
+            },
+            {
+              stars:1,
+              body:"This product sucks",
+              author:"Reali@hater.com"
+            },
+            {
+              stars: 3,
+              body: "I think this gem was just OK, could honestly use more shine, IMO.",
+              author: "JimmyDean@example.org",
+            }
+          ]
+},
+{
+  name:"Zircon",
+  price: 1100,
+  description: "Zircon is our most coveted and sought after gem.",
+  specs: [
+    {
+    faces: 6,
+    color: "#000",
+    rarity: 2,
+    shine: 70
+  }
+],
+  images:[
+            {
+              full: "./img/gem-05.gif",
+              thumb:'./img/gem-05.gif'
+            },
+            {
+              full:'./img/gem-05.gif',
+              thumb:'./img/gem-05.gif'
+            }
+          ],
+  canPurchase: true,
+  soldOut: false,
+  reviews:[
+            {
+              stars: 5,
+              body:"I love this product!",
+              author:"Arnold@homas.com"
+            },
+            {
+              stars:1,
+              body:"This product sucks",
+              author:"Reali@hater.com"
+            },
+            {
+              stars: 1,
+              body: "This gem is WAY too expensive for its rarity value.",
+              author: "turtleguyy@example.org",
+            }
+          ]
+}
+] 
       })()
